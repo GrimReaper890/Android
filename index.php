e408583a5d491eb1210b637594b4ea1298ed52cd
Rizwan atta <rizwanatta890@gmail.com>
2017-06-28 13:42:10 -0700

    test push4

--- index.php
+++ index.php
@@ -6,19 +6,19 @@
 
 //  $fb=json_decode($fb);
  $rid= $fb->entry[0]->messaging[0]->sender->id;
-echo "$rid";
+// echo "$rid";
 // print_r($fb);
 $token = "EAAGForZBCDjUBABxgkSyRQEpimiElQRNUZBKDo9zp3S3o6DDavqqqwEFcToV5DOKyLplOhVRWdkfZB9Nlrm1RqOWZAT8lAMuJAYjnnhDM9rQnQDqm59JZBtBk8hU88rCBxhpXtYebyPVE771uB7PG9xX1y9ZCZB3KyRhR4Alqd9QQZDZD";
 
 $data = array(
     'recipient' => array('id'=>"$rid"),
-    'message' => array('text'=>"oh Hello there! ^_^")
+    'message' => array('text'=>"oh Hello there!")
 );
 $options = array(
     'http'=> array(
             'method'=> 'POST',
              'content' => json_encode($data),
-             'header'=> "Content-Type: application/json\n"
+             'header'=> "Content-Type: application/json"
     )
 );
 $context = stream_context_create($options);
