091f0792c3ab6a2d986fc01a8fc769b343300dba
Rizwan atta <rizwanatta890@gmail.com>
2017-06-28 13:56:07 -0700

    test push7

--- index.php
+++ index.php
@@ -9,19 +9,19 @@
  $rid= $fb->entry[0]->messaging[0]->sender->id;
 // echo "$rid";
 print_r($fb);
-$token = "EAAGForZBCDjUBABxgkSyRQEpimiElQRNUZBKDo9zp3S3o6DDavqqqwEFcToV5DOKyLplOhVRWdkfZB9Nlrm1RqOWZAT8lAMuJAYjnnhDM9rQnQDqm59JZBtBk8hU88rCBxhpXtYebyPVE771uB7PG9xX1y9ZCZB3KyRhR4Alqd9QQZDZD";
+// $token = "EAAGForZBCDjUBABxgkSyRQEpimiElQRNUZBKDo9zp3S3o6DDavqqqwEFcToV5DOKyLplOhVRWdkfZB9Nlrm1RqOWZAT8lAMuJAYjnnhDM9rQnQDqm59JZBtBk8hU88rCBxhpXtYebyPVE771uB7PG9xX1y9ZCZB3KyRhR4Alqd9QQZDZD";
 
-$data = array(
-    'recipient' => array('id'=>"$rid"),
-    'message' => array('text'=>"oh Hello there!")
-);
-$options = array(
-    'http'=> array(
-            'method'=> 'POST',
-             'content' => json_encode($data),
-             'header'=> "Content-Type: application/json"
-    )
-);
-$context = stream_context_create($options);
-file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token",false,$context);
+// $data = array(
+//     'recipient' => array('id'=>"$rid"),
+//     'message' => array('text'=>"oh Hello there!")
+// );
+// $options = array(
+//     'http'=> array(
+//             'method'=> 'POST',
+//              'content' => json_encode($data),
+//              'header'=> "Content-Type: application/json"
+//     )
+// );
+// $context = stream_context_create($options);
+// file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token",false,$context);
  ?>
