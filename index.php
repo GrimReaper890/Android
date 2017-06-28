17975826c492ab89a3d5b61e5b26655ec07e7d00
Rizwan atta <rizwanatta890@gmail.com>
2017-06-28 13:53:01 -0700

    test push6

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php 
  
- print_r($_GET["hub_challenge"]);
+//  print_r($_GET["hub_challenge"]);
  file_put_contents("fb.txt",file_get_contents("php://input"));
  $fb = file_get_contents("fb.txt");
 
  $fb=json_decode($fb);
+ echo $fb->entry[0]->messaging[0]->sender->id;
  $rid= $fb->entry[0]->messaging[0]->sender->id;
 // echo "$rid";
-// print_r($fb);
+print_r($fb);
 $token = "EAAGForZBCDjUBABxgkSyRQEpimiElQRNUZBKDo9zp3S3o6DDavqqqwEFcToV5DOKyLplOhVRWdkfZB9Nlrm1RqOWZAT8lAMuJAYjnnhDM9rQnQDqm59JZBtBk8hU88rCBxhpXtYebyPVE771uB7PG9xX1y9ZCZB3KyRhR4Alqd9QQZDZD";
 
 $data = array(
