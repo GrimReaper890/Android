4b8242203f465ca53187d1c950b5090d301c9272
GrimReaper890 <grimreaperbotmaker890@gmail.com>
2017-07-01 16:48:08 +0500

    error inserter auth

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
  
  print_r($_GET["hub_challenge"]);
-// echo ("me thak geya");
- file_put_contents("fb.txt",file_get_contents("php://input"));
- $fb = file_get_contents('fb.txt');
+// // echo ("me thak geya");
+//  file_put_contents("fb.txt",file_get_contents("php://input"));
+//  $fb = file_get_contents('fb.txt');
 
  ?>
