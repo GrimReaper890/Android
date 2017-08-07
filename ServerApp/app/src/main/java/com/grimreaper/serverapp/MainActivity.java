04e15ca0185af534b856eb618b0818498c329c46
Rizwan Atta <rizwanatta890@gmail.com>
2017-08-07 06:47:46 -0700

    How To add data into xampp db using android VOlley GetParams Method

--- ServerApp/app/src/main/java/com/grimreaper/serverapp/MainActivity.java
+++ ServerApp/app/src/main/java/com/grimreaper/serverapp/MainActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -13,14 +15,33 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnGetDataFromServer;
+    EditText edtTxtName, edtTxtFName, edtTxtPass, edtTxtAddress;
+    String pName, pFName, pPass, pAddress;
+    Button btnRegister,btnGetAllusers;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnGetDataFromServer = (Button) findViewById(R.id.btn_get_server_data);
+
+        edtTxtName = (EditText) findViewById(R.id.edt_txt_name);
+        edtTxtFName = (EditText) findViewById(R.id.edt_txt_fname);
+        edtTxtPass = (EditText) findViewById(R.id.edt_txt_pass);
+        edtTxtAddress = (EditText) findViewById(R.id.edt_txt_adress);
+
+        btnRegister = (Button) findViewById(R.id.btn_register);
+        btnGetAllusers = (Button) findViewById(R.id.btn_get_all_users);
+
+
+
 
         btnGetDataFromServer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +49,31 @@ public class MainActivity extends AppCompatActivity {
                 getDataFromServer();
             }
         });
+
+
+
+        //        listner on the button
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pName = edtTxtName.getText().toString();
+                pFName = edtTxtFName.getText().toString();
+                pPass = edtTxtPass.getText().toString();
+                pAddress = edtTxtAddress.getText().toString();
+
+                if (pName.isEmpty() || pFName.isEmpty() || pPass.isEmpty() || pAddress.isEmpty()){
+                    edtTxtName.setError("Please fill the complete form");
+                    edtTxtFName.setError("Please fill the complete form");
+                    edtTxtPass.setError("Please fill the complete form");
+                    edtTxtAddress.setError("Please fill the complete form");
+                }else{
+//                    db wala kam
+                    RegisterUserOnServer(pName,pFName,pPass,pAddress);
+                }
+
+            }
+        });
+
 
     }
 
@@ -63,5 +109,35 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
         requestQueue.add(stringRequest);
 
+    }
+
+
+    //    we made a method with a request and  a qaeue in it
+    private void RegisterUserOnServer(final String pName, final String pFName, final String pPass, final String pAddress){
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://10.0.2.2:8080/voley/registerUser.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params  = new HashMap<>();
+                params.put("PersonName",pName);
+                params.put("PersonFName",pFName);
+                params.put("PpassWord",pPass);
+                params.put("PAddress",pAddress);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        requestQueue.add(stringRequest);
     }
 }
